6479d3906d142cbe46c72f8940eced23aea5638a
7sDream <i@7sdre.am>
2026-03-19 12:05:49 +0800

    fix: systemd enable greetd, embed short script in recipe

--- files/scripts/cleansddm.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -oue pipefail
-
-# Upstream gdm service, not used
-rm -rf /etc/sddm.conf.d
--- files/scripts/cleanupstream.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -oue pipefail
-
-# Upstream gdm service, not used
-rm /usr/lib/systemd/system/gdm-boot.service
-rm -rf /etc/gdm
